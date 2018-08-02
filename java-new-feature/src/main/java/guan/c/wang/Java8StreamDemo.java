dee66604fab4788472c13098a84338be81e1d77f
Wang <guan.c.wang@accenture.com>
2018-08-02 18:29:58 +0800

    add test file

---
+++ java-new-feature/src/main/java/guan/c/wang/Java8StreamDemo.java
@@ -0,0 +1,37 @@
+package guan.c.wang;
+
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by guan.c.wang on 8/2/2018.
+ */
+public class Java8StreamDemo {
+
+
+    private final IntPredicate predicate2 = i -> i< 10;
+    private final IntPredicate predicate = i -> i > 0;
+
+    private final Predicate<Integer>  predicate_3 = i -> i > 0;
+
+    private final  Predicate<String>    predicate_4 = s -> s.contains("s");
+
+    public void  basicIntStream(IntStream stream){
+
+        stream.filter(predicate).filter(predicate2).count();
+
+    }
+
+    public void basicStream(Stream stream){
+        stream.filter(predicate_3).count();
+
+    }
+
+    public void bacicStream2(Stream stream){
+        stream.filter(predicate_4).count();
+    }
+
+
+}
---
+++ java-new-feature/src/test/java/guan/c/wang/Java8StreamDemoTest.java
@@ -0,0 +1,48 @@
+package guan.c.wang;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * @auther guan.c.wang
+ * @date 8/2/2018.
+ * @see
+ */
+public class Java8StreamDemoTest {
+
+
+    private IntStream intStream;
+    private Java8StreamDemo demo;
+    private Stream stream;
+    private Stream stream2;
+
+    @Before
+    public void setUp() throws Exception {
+        demo = new Java8StreamDemo();
+        intStream = IntStream.range(0, 100);
+        stream = Stream.of(1,2,3);
+        stream2 = Stream.of("a", "b", "c");
+    }
+
+    @Test
+    public void basicIntStream() throws Exception {
+        demo.basicIntStream(intStream);
+    }
+
+    @Test
+    public void basicStream() throws Exception {
+        demo.basicStream(stream);
+    }
+
+    @Test
+    public void bacicStream2() throws Exception {
+        demo.bacicStream2(stream2);
+    }
+
+}
