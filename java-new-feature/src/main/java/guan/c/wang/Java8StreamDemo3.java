b307743ebf749907c2d776deaa48a477815f9154
Wang <guan.c.wang@accenture.com>
2018-08-03 00:18:44 +0800

    update test

---
+++ java-new-feature/src/main/java/guan/c/wang/Java8StreamDemo3.java
@@ -0,0 +1,36 @@
+package guan.c.wang;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @auther guan.c.wang
+ * @date 8/2/2018.
+ * @see  java.util.stream.Stream
+ */
+class Foo<T> {
+    public Foo(String name) {
+        this.name = name;
+    }
+
+    public String name;
+    public List<T> bars = new ArrayList<T>();
+        }
+class Bar {
+    public Bar(String name) {
+        this.name = name;
+    }
+
+    public String name;
+        }
+
+public class Java8StreamDemo3 {
+
+    public void flatMap(List<Foo<Bar>> foos){
+        Stream<Bar> barStream = foos.stream().flatMap(f -> f.bars.stream());
+
+
+    }
+
+}
---
+++ java-new-feature/src/test/java/guan/c/wang/Java8StreamDemo3Test.java
@@ -0,0 +1,35 @@
+package guan.c.wang;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * @auther guan.c.wang
+ * @date 8/3/2018.
+ * @see
+ */
+public class Java8StreamDemo3Test {
+
+    private Stream<Foo> foos;
+
+    @Before
+    public void setUp() throws Exception {
+        // initilize the foos
+        foos = IntStream.range(0, 10).mapToObj(i -> new Foo(" Foo-Name-Id" + i)).peek(f -> {
+            IntStream.range(0, 10).mapToObj(i -> new Bar("Bar-name-id " + i)).forEach(f.bars::add);
+        });
+
+
+    }
+
+    @Test
+    public void flatMap() throws Exception {
+       assertEquals(100 ,foos.flatMap( f -> f.bars.stream()).count() );
+    }
+
+}
