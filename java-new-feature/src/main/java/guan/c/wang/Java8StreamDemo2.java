2cd7cfd40556a4f4aa5275dedc4063a7c5d9127c
Wang <guan.c.wang@accenture.com>
2018-08-02 19:01:22 +0800

    add two files

---
+++ java-new-feature/src/main/java/guan/c/wang/Java8StreamDemo2.java
@@ -0,0 +1,42 @@
+package guan.c.wang;
+
+import java.util.IntSummaryStatistics;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @auther guan.c.wang
+ * @date 8/2/2018.
+ * @see
+ */
+
+
+public class Java8StreamDemo2 {
+
+    public Stream<People> collectAbove18(Stream<People> peoples ){
+        return peoples.filter( p -> p.age > 18);
+    }
+
+
+    public Double collectAvgOfAge(Stream<People> peoples ){
+       return  peoples.collect(Collectors.averagingInt(p -> p.age));
+
+    }
+
+
+    public IntSummaryStatistics collectSumOfAge(Stream<People> peoples ){
+       return  peoples.collect(Collectors.summarizingInt(p -> p.age));
+    }
+
+    public String collectJoinName(Stream<People> peoples ){
+
+      return   peoples.map(p -> p.name ).collect(Collectors.joining("and" , "Both" , " are chinese "));
+    }
+
+
+    public Map<String, Integer> collectMap(Stream<People> peoples ){
+       return  peoples.collect(Collectors.toMap(p -> p.name , p-> p.age));
+
+    }
+}
---
+++ java-new-feature/src/main/java/guan/c/wang/People.java
@@ -0,0 +1,28 @@
+package guan.c.wang;
+
+/**
+ * @auther guan.c.wang
+ * @date 8/2/2018.
+ * @see
+ */
+public class People {
+
+    public String name ;
+    public int age ;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
